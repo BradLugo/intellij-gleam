e784ffbd0f863e2823f08690adc598f4de4ecc7b
GitHub Action <action@github.com>
2024-05-24 04:24:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/bradlugo/intellijgleam/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/bradlugo/intellijgleam/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bradlugo.intellijgleam.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
