22b1bf8af59e81750b2c1134f0f219a24c0c9a5c
Brad Lugo <brad@bradlugo.com>
2024-05-26 19:16:10 -0600

    feat: add basic Gleam formatting via the LSP

--- src/main/kotlin/com/github/bradlugo/intellijgleam/lsp/GleamLspServerDescriptor.kt
+++ src/main/kotlin/com/github/bradlugo/intellijgleam/lsp/GleamLspServerDescriptor.kt
@@ -6,8 +6,14 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
+import com.intellij.platform.lsp.api.customization.LspFormattingSupport
 
 internal class GleamLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Gleam") {
     override fun isSupportedFile(file: VirtualFile) = file.extension == "gleam"
     override fun createCommandLine() = GeneralCommandLine("gleam", "lsp")
+    override val lspFormattingSupport = object : LspFormattingSupport() {
+        override fun shouldFormatThisFileExclusivelyByServer(
+            file: VirtualFile, ideCanFormatThisFileItself: Boolean, serverExplicitlyWantsToFormatThisFile: Boolean
+        ) = file.extension == "gleam"
+    }
 }
