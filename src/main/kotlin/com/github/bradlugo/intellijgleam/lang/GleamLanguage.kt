7d7bbd0e261d537eafb36ae1f6d912efadb019a0
Brad Lugo <brad@bradlugo.com>
2025-01-14 23:55:58 -0800

    chore: get updates from intellij-platform-plugin-template

--- src/main/kotlin/com/github/bradlugo/intellijgleam/lang/GleamLanguage.kt
+++ src/main/kotlin/com/github/bradlugo/intellijgleam/lang/GleamLanguage.kt
@@ -3,7 +3,5 @@ package com.github.bradlugo.intellijgleam.lang
 import com.intellij.lang.Language
 
 object GleamLanguage : Language("Gleam") {
-    override fun isCaseSensitive() = true
-
-    override fun getDisplayName() = "Gleam"
+    private fun readResolve(): Any = GleamLanguage
 }
